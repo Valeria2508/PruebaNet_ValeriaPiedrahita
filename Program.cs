914724fea2364b7a7ed1c4a2b468093de78b1fc9
val <valeria@gmail.com>
2024-10-07 09:24:22 -0500

    feat: add to the program jwt, home pages, padlock

--- Program.cs
+++ Program.cs
@@ -17,12 +17,75 @@ var conectionDb = $"server={db_host};port={db_port};database={db_database};uid={
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(conectionDb, ServerVersion.Parse("8.0.20-mysql")));
 
+builder.Services.AddSingleton<Utilities>(); //add singleton use the utulities
+
+
+
+
+//JWT configuracion
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER"),
+        ValidateAudience = false, // false because the audience is public
+        ValidAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_KEY")!))
+    };
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//el candadito del swagger
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "TechStore API",
+        Version = "v1",
+        Description = "API for managing a Tech store database. This version includes basic CRUD operations for users, products and categories.",
+        Contact = new OpenApiContact
+        {
+            Name = "Valeria Piedrahita Arbelaez" ,
+            Email = "valetqm2002@gmail.com",
+        }
+    });
+
+    c.EnableAnnotations();
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
 
 var app = builder.Build();
 
@@ -36,6 +99,34 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// welcome page
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        var htmlContent = @"
+        <html>
+            <head>
+                <title>Hotels API -Prueba .Net-</title>
+            </head>
+            <body style='font-family: Arial, sans-serif; background-color: #f4f4f4; text-align: center; height: 90%; display: flex; flex-direction: column; justify-content: center; align-items: center;'>
+                <h1 style='color: #333; font-size: 36px;'>Welcome to Hotel</h1>
+                <a href='/swagger' style='color: #007bff; text-decoration: none;'> Click here to Swagger documentation</a>
+            </body>
+        </html>";
+
+        context.Response.ContentType = "text/html";
+        await context.Response.WriteAsync(htmlContent);
+    }
+    else
+    {
+        await next();
+    }
+});
+
+// fin de wolcome pages
 
 app.MapControllers();
 
