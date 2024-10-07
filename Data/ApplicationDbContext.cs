60f6f325333069a162f8209966c6599a498680cd
val <valeria@gmail.com>
2024-10-07 06:52:54 -0500

    feat: finish configuring the db

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PruebaNet_ValeriaPiedrahita.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using PruebaNet_ValeriaPiedrahita.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//config db
+Env.Load();
+
+var db_host = Environment.GetEnvironmentVariable("DB_HOST");
+var db_port = Environment.GetEnvironmentVariable("DB_PORT");
+var db_database = Environment.GetEnvironmentVariable("DB_DATABASE");
+var db_username = Environment.GetEnvironmentVariable("DB_USERNAME");
+var db_password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var conectionDb = $"server={db_host};port={db_port};database={db_database};uid={db_username};password={db_password}";
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(conectionDb, ServerVersion.Parse("8.0.20-mysql")));
 
 // Add services to the container.
 
